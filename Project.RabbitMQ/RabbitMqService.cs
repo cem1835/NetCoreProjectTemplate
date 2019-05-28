64aa3ac3b3a1841717d8c8bc89cf383bf4c49340
cem ve tuğut <cem.tugut@hotmail.com.tr>
2019-05-28 17:38:05 +0300

    Rabbit mq connection added

---
+++ Project.RabbitMQ/RabbitMqService.cs
@@ -0,0 +1,30 @@
+﻿using RabbitMQ.Client;
+using System;
+
+namespace Project.RabbitMQ
+{
+    public class RabbitMqService
+    {
+        private static IConnection _connection;
+
+        private RabbitMqService()
+        {
+
+        }
+        public static IConnection GetConnection()
+        {
+            if (_connection == null)
+            {
+                ConnectionFactory connectionFactory = new ConnectionFactory()
+                {
+                    HostName="",
+                    UserName="",
+                    Password=""
+                };
+
+                _connection = connectionFactory.CreateConnection();
+            }
+            return _connection;
+        }
+    }
+}
